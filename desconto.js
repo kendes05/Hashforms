b8d9a6d16176ab575fc1448723a3aefab8404423
kendes05 <ptimbo652@gmail.com>
2025-03-29 23:49:20 -0300

    js desconto

---
+++ desconto.js
@@ -0,0 +1,33 @@
+const input = document.getElementById("discount-input");
+const buttonDiscount = document.getElementById("apply-discount-button");
+const spanApply = document.getElementById("apply-discount-span");
+const icon = document.getElementById("toggle-icon");
+const spanDiscount = document.getElementById("discount-span");
+const totalPrice = document.getElementById("total-price");
+
+spanApply.addEventListener("click", () => {
+  if (input.style.display === "none" || input.style.display === "") {
+    input.style.display = "block";
+    buttonDiscount.style.display = "block";
+    icon.classList.add("bx-chevron-up");
+    icon.classList.remove("bxs-chevron-down");
+  } else if (input.style.display === "block") {
+    input.style.display = "none";
+    buttonDiscount.style.display = "none";
+    icon.classList.remove("bx-chevron-up");
+    icon.classList.add("bxs-chevron-down");
+  }
+});
+
+buttonDiscount.addEventListener("click", () => {
+  if (input.value.trim() !== "") {
+    spanDiscount.style.display = "block";
+    spanDiscount.textContent = `Desconto de ${input.value.toUpperCase()} aplicado!`;
+    totalPrice.textContent = "Preço Total: R$ 1080.00";
+    input.value = "";
+  } else {
+    alert("Cupom de desconto inválido");
+  }
+});
+
+//bxs-chevron-down
